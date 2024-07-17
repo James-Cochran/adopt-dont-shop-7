9a8ab832123ad37980f599240b731f136af98bd2
James-Cochran <jameslikesfruit@gmail.com>
2024-07-17 13:38:47 -0400

    paired synch user story 11 completion.

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -17,4 +17,38 @@ RSpec.describe "the admin shelters index page" do
         expect(page).to have_content("Mystery Building")
         expect(page).to have_content("Velma's Pet Emporium") 
     end
+
+    #User Story 11
+    it "has a section for Shelters with pending applications" do
+        shelter1 = Shelter.create!(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+        shelter2 = Shelter.create!(name: "Fred and Daphne's Super Shelter", city: "Irvine CA", foster_program: false, rank: 10)
+        shelter3 = Shelter.create!(name: "Velma's Pet Emporium", city: "Irvine CA", foster_program: false, rank: 7)
+        pet1 = Pet.create!(name: "Scooby", age: 2, breed: "Great Dane", adoptable: true, shelter_id: shelter1.id)
+        pet2 = Pet.create!(name: "Scrappy", age: 1, breed: "Not So Great Dane", adoptable: true, shelter_id: shelter2.id)
+        pet3 = Pet.create!(name: "Blue", age: 3, breed: "Blue Dog", adoptable: true, shelter_id: shelter1.id)
+        app1 = Application.create!(name: "Shaggy",
+                                    street_address: "1234 Bobs rd",
+                                    city: "Denver",
+                                    state: "Colorado",
+                                    zip_code: "80220",
+                                    description: "I want a dog!",
+                                    status: "Pending")
+        app2 = Application.create!(name: "Fred",
+                                    street_address: "1234 Fred rd",
+                                    city: "Boulder",
+                                    state: "Colorado",
+                                    zip_code: "80221",
+                                    description: "I NEED a dog!",
+                                    status: "Pending")
+        PetApplication.create!(application: app1, pet: pet1)
+        PetApplication.create!(application: app2, pet: pet2) 
+    
+        visit '/admin/shelters'
+        
+        within("#pending_apps") do
+            expect(page).to have_content(shelter1.name)
+            expect(page).to have_content(shelter2.name)
+            expect(page).to_not have_content(shelter3.name)
+        end
+    end
 end
