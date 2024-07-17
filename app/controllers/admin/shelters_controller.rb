9209049c0099bd1694f6b6621f2a4d1df7f395eb
Lance Nelson <lancen04@gmail.com>
2024-07-17 18:27:36 -0400

    completed code formatting cleanup as well as added model test for Shelter.pending_apps class method

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,7 +1,6 @@
 class Admin::SheltersController < ApplicationController
-
-    def index
-        @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
-        @pending_apps = Shelter.pending_apps
-    end
+  def index
+    @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY name DESC")
+    @pending_apps = Shelter.pending_apps
+  end
 end
--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -1,43 +1,42 @@
 require "rails_helper"
 
 RSpec.describe "application creation" do
-    it "creates a new application" do
-        
+  it "creates a new application" do    
+  	visit "/applications/new"
+
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Street address")
+    expect(page).to have_content("City")
+    expect(page).to have_content("State")
+    expect(page).to have_content("Zip code")
+    expect(page).to have_content("Description")  
+  end
+
+  describe "the application create" do
+    context "if given valid data" do
+      it "creates the application" do
         visit "/applications/new"
 
-        expect(page).to have_content("Name")
-        expect(page).to have_content("Street address")
-        expect(page).to have_content("City")
-        expect(page).to have_content("State")
-        expect(page).to have_content("Zip code")
-        expect(page).to have_content("Description")
-    
+        fill_in "name", with: "Shaggy"
+        fill_in "street_address", with: "1234 Bobs rd"
+        fill_in "city", with: "Denver"
+        fill_in "state", with: "Colorado"
+        fill_in "zip_code", with: "80220"
+        fill_in "description", with: "I want a dog!"
+
+        click_button "Save"
+
+        application_id = Application.last.id
+        expect(current_path).to eq "/applications/#{application_id}"
+
+        expect(page).to have_content("Name: Shaggy")
+        expect(page).to have_content("Street Address: 1234 Bobs rd")
+        expect(page).to have_content("City: Denver")
+        expect(page).to have_content("State: Colorado")
+        expect(page).to have_content("Zip Code: 80220")
+        expect(page).to have_content("Description: I want a dog!")
+        expect(page).to have_content("Status: In Progress")
+      end
     end
-
-    describe "the application create" do
-        context "given valid data" do
-            it "creates the application" do
-                visit "/applications/new"
-
-                fill_in "name", with: "Shaggy"
-                fill_in "street_address", with: "1234 Bobs rd"
-                fill_in "city", with: "Denver"
-                fill_in "state", with: "Colorado"
-                fill_in "zip_code", with: "80220"
-                fill_in "description", with: "I want a dog!"
-              
-
-                click_button "Save"
-                application_id = Application.last.id                           
-                expect(current_path).to eq "/applications/#{application_id}"
-                expect(page).to have_content("Name: Shaggy")
-                expect(page).to have_content("Street Address: 1234 Bobs rd")
-                expect(page).to have_content("City: Denver")
-                expect(page).to have_content("State: Colorado")
-                expect(page).to have_content("Zip Code: 80220")
-                expect(page).to have_content("Description: I want a dog!")
-                expect(page).to have_content("Status: In Progress")
-            end
-        end
-    end
+  end
 end
