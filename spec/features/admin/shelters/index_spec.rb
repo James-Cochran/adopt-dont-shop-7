4425fb2f95d2be31634e4b444d44a898d064b38f
James-Cochran <jameslikesfruit@gmail.com>
2024-07-17 12:36:26 -0400

    refactor admin/shelters/index spec to allign with user story. Created view, added sql to controller index method.

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe "the admin shelters index page" do
         shelter3 = Shelter.create!(name: "Velma's Pet Emporium", city: "Irvine CA", foster_program: false, rank: 7)
 
         visit '/admin/shelters'
-
-        expect(shelter2.name).to appear_before(shelter1.name)
-        expect(shelter2.name).to appear_before(shelter3.name)
-        expect(shelter1.name).to appear_before(shelter3.name)
+        
+        expect(shelter3.name).to appear_before(shelter1.name)
+        expect(shelter3.name).to appear_before(shelter2.name)
+        expect(shelter1.name).to appear_before(shelter2.name)
         
         expect(page).to have_content("Fred and Daphne's Super Shelter")
         expect(page).to have_content("Mystery Building")
